5b9bc2b2c9a1dd99420edefc397eeb1dd5102135
wuwix <you@example.com>
2018-07-02 11:39:25 +0200

    header fixes, triangles, dear vicky element, scroll elements

--- js/form_post_request.js
+++ js/form_post_request.js
@@ -1,19 +1,25 @@
 $(document).ready(function(){
-    $(".link-element").click(function(){
+    $(".link-element.question").click(function(){
+        var mail = $("input#inp")[0].value;
+        var question = $("input#inp2")[0].value;
+        if (mail !==  "" && question !== "") {
             $.ajax({
-            url: "http://httpbin.org/post",
-            data: {
-            mail: $("input#inp")[0].value,
-            question: $("input#inp2")[0].value,
-            },
-            type: 'POST',
-            success: function (resp) {
-                alert("your e-mail: " + JSON.stringify(resp.form.mail)
-                + " and question:" + JSON.stringify(resp.form.question));
-            },
-            error: function(e) {
-                alert('Error: '+ JSON.stringify(e.status));
-            }  
-        });
+                url: "http://httpbin.org/post",
+                data: {
+                mail: $("input#inp")[0].value,
+                question: $("input#inp2")[0].value,
+                },
+                type: 'POST',
+                success: function (resp) {
+                    alert("your e-mail: " + JSON.stringify(resp.form.mail)
+                    + " and question:" + JSON.stringify(resp.form.question));
+                },
+                error: function(e) {
+                    alert('Error: '+ JSON.stringify(e.status));
+                }  
+            });
+        } else {
+            alert('All fields should be filled.');
+        }
     });
 });
